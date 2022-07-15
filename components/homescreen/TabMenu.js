7c4339d9bdee018af09f090ab53d49d30e41b3d2
Sneaky <30368966+WisdomAmuzu@users.noreply.github.com>
2022-07-15 14:38:18 +0100

    Update and rename tabmenu.js to TabMenu.js

--- components/homescreen/TabMenu.js
+++ components/homescreen/TabMenu.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const tabmenu = () => {
+const TabMenu = () => {
   return (
     <View style={styles.container}>
       <Text>TabMenu</Text>
@@ -19,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default TabMenu;
