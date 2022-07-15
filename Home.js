3dc312d17d300110e6342ed583606105ae027596
Sneaky <30368966+WisdomAmuzu@users.noreply.github.com>
2022-07-15 14:41:23 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Header } from "homescreen/components/Header";
+import { Activity } from "homescreen/components/Activity";
+import { MainMenu } from "homescreen/components/MainMenu";
+import { TabMenu } from "homescreen/components/TabMenu";
 
 const Home = () => {
   return (
     <View>
-        
+        <Header />
+        <Activity />
+        <MainMenu />
+        <TabMenu />
     </View>
   );
 };
